cb6a59fa8e417e35a3130e740c73868d4922c4e6
Handy7430 <handb7430@gmail.com>
2026-03-19 14:00:54 +0000

    Create news.js

---
+++ api/news.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  res.status(200).json([
+    {
+      title: "MASI en hausse aujourd'hui",
+      link: "#"
+    },
+    {
+      title: "ارتفاع بورصة الدار البيضاء",
+      link: "#"
+    }
+  ]);
+}
